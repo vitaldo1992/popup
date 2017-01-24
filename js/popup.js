1ee43c1719d880d2c2e5651f2376ebdbd239746c
Vitaldo <vitaliy.duvalko@gmail.com>
2017-01-24 13:49:12 +0200

    created multi-email-input

--- js/popup.js
+++ js/popup.js
@@ -15,5 +15,34 @@ $(function() {
 				$(element).find('.label-text').text('Прикрепить тарифный план - FREEDOM')
 			}
 		}, 10)
-	})
+	});
+	$('#demo').multiple_emails({
+		checkDupEmail: true
+	});
+
+	restrictPlaceHolder();
+
+	function restrictPlaceHolder() {
+		if ($('.email_name').length>0) {
+			$('.multiple_emails-input.text-left').attr("placeholder", '')
+			$('.multiple_emails-input.text-left').css('margin-top', '3px')
+		} else {
+			$('.multiple_emails-input.text-left').attr("placeholder", 'Вы можете ввести до 5 e-mail через пробел')
+			$('.multiple_emails-input.text-left').css('margin-top', '35px')
+		}
+	}
+	function restrictEmail (){
+		if ($('.email_name').length>4) {
+			$('.multiple_emails-input.text-left').attr("disabled", true)
+		} else {
+			$('.multiple_emails-input.text-left').attr("disabled", false)
+		}
+	}
+	$('.multiple_emails-input.text-left').on('keyup focusout focus', restrictEmail);
+	$('.multiple_emails-input.text-left').on('keyup focusout focus', restrictPlaceHolder);
+	$('.form-layout').on('click', restrictEmail)
+	// $('#current_emails').text($('#demo').val());
+	// $('#demo').change( function(){
+	// 	$('#current_emails').text($(this).val());
+	// });
 })
